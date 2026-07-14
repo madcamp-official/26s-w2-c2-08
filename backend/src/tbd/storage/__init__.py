39703bcc22754d90665b3a9043a8cc36c383a906
jungjun0708 <jungjun0708@gmail.com>
2026-07-14 14:32:34 +0900

    백엔드: Storage protocol과 논리 키 추가

---
+++ backend/src/tbd/storage/__init__.py
@@ -0,0 +1,35 @@
+"""Provider-neutral private object storage primitives."""
+
+from tbd.storage.contracts import (
+    Storage,
+    StorageAppendResult,
+    StorageConflictError,
+    StorageDeleteResult,
+    StorageError,
+    StorageIntegrityError,
+    StorageKey,
+    StorageNamespace,
+    StorageNotFoundError,
+    StorageObject,
+    StorageOffsetMismatchError,
+    StorageRangeError,
+    StorageUnavailableError,
+    sha256_bytes,
+)
+
+__all__ = [
+    "Storage",
+    "StorageAppendResult",
+    "StorageConflictError",
+    "StorageDeleteResult",
+    "StorageError",
+    "StorageIntegrityError",
+    "StorageKey",
+    "StorageNamespace",
+    "StorageNotFoundError",
+    "StorageObject",
+    "StorageOffsetMismatchError",
+    "StorageRangeError",
+    "StorageUnavailableError",
+    "sha256_bytes",
+]
